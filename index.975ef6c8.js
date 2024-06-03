2664f3df8ad5569b41616ad4f68be69d94293e86
Anjitha k <147159980+foodspecious@users.noreply.github.com>
2024-06-03 11:28:16 +0000

    Updates

--- index.975ef6c8.js
+++ index.975ef6c8.js
@@ -27087,7 +27087,7 @@ var _stylesCss = require("./styles.css");
  *
  * If you don't have one of the social sites listed, leave it as an empty string.
  */ const siteProps = {
-    name: "ANJITHA k",
+    name: "ANJITHA K",
     title: "WEB DEVELOPMENT FRESHER",
     email: "anjithakarunakaran14@gmail,com",
     gitHub: "foodspecious",
