cc1444a883eab06866ddabdd5e2eaad8ac16e0ae
foodspecious <vihaanju14@gmail.com>
2024-06-03 16:06:47 +0530

    commit

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/bg.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "image";
 
 /**
  * Project list
@@ -31,28 +31,22 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Inscribe - a dynamic note management web application",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "A note management web application,with features such as nested notes,collaboration,a feature-rich editor,templates and exporting notes.",
+    url: "https://inscribe-topaz.vercel.app",
   },
   {
-    title: "Web Development for Beginners",
+    title: "PIZZA HOUSE- a pizza ordering PHP website with user authentication ",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "A website which i creates as my semester project.This website has auser authentication system made using XAMP,in which users can order pizza and get a invoice.",
+    url: "https://github.com/foodspecious/PizzaHouse",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    url: "https://github.com/foodspecious/Resume-Anjitha",
   },
 ];
 
